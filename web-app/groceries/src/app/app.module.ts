1db1d65281628896b223a2b13442706e4ef21129
Yannick Bellerose <yan-bel@hotmail.com>
2021-06-08 14:11:21 -0400

    the sales page work -- need to be integrated to the api

--- web-app/groceries/src/app/app.module.ts
+++ web-app/groceries/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
-
 import { SalesModule } from './sales/sales.module';
 import { StoresModule } from './stores/stores.module';
 
--- web-app/groceries/src/app/stores/stores-home/stores-home.component.ts
+++ web-app/groceries/src/app/stores/stores-home/stores-home.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoresHomeComponent implements OnInit {
 
+  stores: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    let store1 = {name: 'Test1', openAt: 11, closeAt: 20, address: '1111 Test Street', phone: '123 113-412'};
+    let store2 = {name: 'Test2', openAt: 15, closeAt: 22, address: '2222 Test Drive', phone: '222 123-432'};
+    this.stores = [store1, store2];
+  }
+
+  storeIsOpen(openAt: number, closeAt:number): boolean{
+    var date = new Date();
+    return openAt <= date.getHours() && date.getHours() < closeAt;
   }
 
 }
