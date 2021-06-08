3f40e9c84d3ad550ba1c058cf2f6cc89f4685ad0
Yannick Bellerose <yan-bel@hotmail.com>
2021-06-08 11:55:34 -0400

    working router

--- web-app/groceries/src/app/app.module.ts
+++ web-app/groceries/src/app/app.module.ts
@@ -3,22 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowseModule } from './browse/browse.module';
 import { HomeModule } from './home/home.module';
+
 import { SalesModule } from './sales/sales.module';
 import { StoresModule } from './stores/stores.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BrowseModule,
-    HomeModule,
-    StoresModule,
-    SalesModule
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoresModule,
+    SalesModule,
+    HomeModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
